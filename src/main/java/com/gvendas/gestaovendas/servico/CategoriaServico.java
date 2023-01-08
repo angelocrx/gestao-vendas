358e2758784ce97883373da0d61fc4c29e0927bb
Angelo Cassio Ramos Xavier <cassio.ramos@centralinf.com.br>
2023-01-08 11:45:25 -0300

    First commit

---
+++ src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
@@ -0,0 +1,25 @@
+package com.gvendas.gestaovendas.servico;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gvendas.gestaovendas.entidades.Categoria;
+import com.gvendas.gestaovendas.repositorio.CategoriaRepositorio;
+
+@Service
+public class CategoriaServico {
+
+	@Autowired // Auto Inject
+	private CategoriaRepositorio categoriaRepositorio;
+
+	public List<Categoria> listarTodas() {
+		return categoriaRepositorio.findAll();
+	}
+
+	public Optional<Categoria> buscarPorId(Long id) {
+		return categoriaRepositorio.findById(id);
+	}
+}
